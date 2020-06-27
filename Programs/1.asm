5388953ddaca442110bb167b3cf753b866c914a0
Jishan Shaikh <30091032+jishanshaikh4@users.noreply.github.com>
2020-06-27 12:07:37 +0530

    Update 1.asm

--- Programs/1.asm
+++ Programs/1.asm
@@ -1,1 +1,27 @@
-
+data segment
+a dw 5e6eh
+b dw 0a5ah
+c dw ? 
+cr dw ?
+data ends
+ 
+code segment
+assume cs:code,ds:data
+start: mov cx,0000h
+mov ax,data
+mov ds,ax 
+mov dx,a
+mov bx,b
+mov al,bl
+add al,dl
+mov ah,bh
+adc ah,dh 
+jnc next
+inc cx 
+next: mov [di+4],ax   
+mov [di+6],cx
+hlt 
+ code ends
+end start
+end next
+Output
